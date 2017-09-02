c1705b2be6de39968094b608fb7898eee520ab1a
Mahesh Mehandiratta <mahesh.mehandiratta@nagarro.com>
2017-09-02 22:13:18 +0530

    add todo functionality added

--- Assignment/assignment3/public/script.js
+++ Assignment/assignment3/public/script.js
@@ -31,20 +31,20 @@ function addTodoElements(todos_data_json) {
     DELETEDparent.innerHTML="";
 
     Object.keys(todos).forEach(function (key) {
-           var todo_element = createTodoElement(key,todos[key]);
-           if(todos[key].status=="ACTIVE")
-           {
-               ACTIVEparent.appendChild(todo_element);
-           }
-           else if(todos[key].status=="COMPLETE")
-           {
-               COMPLETEparent.appendChild(todo_element);
-           }
-           else if(todos[key].status=="DELETED")
-           {
-               DELETEDparent.appendChild(todo_element);
-           }
-       });
+        var todo_element = createTodoElement(key,todos[key]);
+        if(todos[key].status=="ACTIVE")
+        {
+            ACTIVEparent.appendChild(todo_element);
+        }
+        else if(todos[key].status=="COMPLETE")
+        {
+            COMPLETEparent.appendChild(todo_element);
+        }
+        else if(todos[key].status=="DELETED")
+        {
+            DELETEDparent.appendChild(todo_element);
+        }
+    });
 };
 
 function createTodoElement(id,todo_object) {
@@ -57,4 +57,25 @@ function createTodoElement(id,todo_object) {
     return todo_element;
 };
 
+function addTodoAJAX() {
+    var value = document.getElementById(NEW_TODO_INPUT).value;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","/api/todos",true);
+    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
 
+    var data = "todo_title="+encodeURI(value);
+
+    xhr.onreadystatechange = function () {
+
+        if(xhr.readyState == RESPONSE_DONE){
+
+            if(xhr.status == STATUS_OK){
+                addTodoElements(xhr.responseText);
+            }
+            else{
+                console.log(xhr.responseText);
+            }
+        }
+    };
+    xhr.send(data);
+};
